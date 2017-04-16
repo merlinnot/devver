98cc11c9988b13531dc7d86b07c736f4fa4623b9
Natan Sągol <m@merlinnot.com>
2017-04-16 15:59:35 +0200

    Update VS Code extensions.

--- modules/tool/vscode/extensions.sh
+++ modules/tool/vscode/extensions.sh
@@ -20,25 +20,24 @@ function vsext {
   echo "installed /root/.vscode/extensions/$publisher.$extension-$version"
 }
 
-vsext formulahendry auto-rename-tag 0.0.8
+vsext formulahendry auto-rename-tag 0.0.11
 vsext wmaurer change-case 1.0.0
-vsext naumovs color-highlight 1.2.1
-vsext dbaeumer vscode-eslint 1.1.0
+vsext naumovs color-highlight 2.0.1
+vsext dbaeumer vscode-eslint 1.2.8
 vsext waderyan gitblame 1.3.0
-vsext donjayamanne githistory 0.1.2
-vsext codezombiech gitignore 0.4.0
-vsext abusaidm html-snippets 0.0.18
-vsext buzzfrog imagepreview 0.5.1
-vsext xabikos JavaScriptSnippets 1.2.0
+vsext donjayamanne githistory 0.2.0
+vsext codezombiech gitignore 0.5.0
+vsext abusaidm html-snippets 0.1.0
+vsext xabikos JavaScriptSnippets 1.4.0
 vsext akamud vscode-javascript-snippet-pack 0.1.5
-vsext eg2 vscode-npm-script 0.1.2
-vsext christian-kohler npm-intellisense 0.1.4
-vsext christian-kohler path-intellisense 1.1.0
+vsext eg2 vscode-npm-script 0.1.9
+vsext christian-kohler npm-intellisense 1.3.0
+vsext christian-kohler path-intellisense 1.2.0
 vsext timothymclane react-redux-es6-snippets 2.0.2
-vsext xabikos ReactSnippets 1.2.0
-vsext jakob101 RelativePath 0.11.0
-vsext humao rest-client 0.11.0
-vsext robinbentley sass-indented 1.2.1
-vsext cssho vscode-svgviewer 1.2.0
+vsext xabikos ReactSnippets 1.2.1
+vsext jakob101 RelativePath 0.12.0
+vsext humao rest-client 0.14.0
+vsext robinbentley sass-indented 1.3.1
+vsext cssho vscode-svgviewer 1.4.0
 vsext hoovercj vscode-dimmer 2.1.0
-vsext polymer polymer-ide 0.2.0
+vsext polymer polymer-ide 0.5.0
