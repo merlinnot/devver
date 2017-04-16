e4bf05843b366c0d8bd0ceea2d7d6f70a3977823
Natan Sągol <m@merlinnot.com>
2017-04-16 14:28:11 +0000

    Install Haskelly and sort vscode extensions.

--- modules/tool/vscode/extensions.sh
+++ modules/tool/vscode/extensions.sh
@@ -20,26 +20,27 @@ function vsext {
   echo "installed /root/.vscode/extensions/$publisher.$extension-$version"
 }
 
-vsext formulahendry auto-rename-tag 0.0.11
-vsext wmaurer change-case 1.0.0
-vsext naumovs color-highlight 2.0.1
-vsext dbaeumer vscode-eslint 1.2.8
-vsext waderyan gitblame 1.3.0
-vsext donjayamanne githistory 0.2.0
-vsext codezombiech gitignore 0.5.0
 vsext abusaidm html-snippets 0.1.0
-vsext xabikos JavaScriptSnippets 1.4.0
 vsext akamud vscode-javascript-snippet-pack 0.1.5
-vsext eg2 vscode-npm-script 0.1.9
 vsext christian-kohler npm-intellisense 1.3.0
 vsext christian-kohler path-intellisense 1.2.0
-vsext timothymclane react-redux-es6-snippets 2.0.2
-vsext xabikos ReactSnippets 1.2.1
-vsext jakob101 RelativePath 0.12.0
-vsext humao rest-client 0.14.0
-vsext robinbentley sass-indented 1.3.1
+vsext codezombiech gitignore 0.5.0
 vsext cssho vscode-svgviewer 1.4.0
+vsext dbaeumer vscode-eslint 1.2.8
+vsext donjayamanne githistory 0.2.0
+vsext eg2 vscode-npm-script 0.1.9
+vsext formulahendry auto-rename-tag 0.0.11
 vsext hoovercj vscode-dimmer 2.1.0
-vsext polymer polymer-ide 0.5.0
-vsext polymer polymer-ide 0.5.0
+vsext humao rest-client 0.14.0
+vsext jakob101 RelativePath 0.12.0
+vsext naumovs color-highlight 2.0.1
 vsext PeterJausovec vscode-docker 0.0.13
+vsext polymer polymer-ide 0.5.0
+vsext polymer polymer-ide 0.5.0
+vsext robinbentley sass-indented 1.3.1
+vsext timothymclane react-redux-es6-snippets 2.0.2
+vsext UCL haskelly 0.4.20
+vsext waderyan gitblame 1.3.0
+vsext wmaurer change-case 1.0.0
+vsext xabikos JavaScriptSnippets 1.4.0
+vsext xabikos ReactSnippets 1.2.1
