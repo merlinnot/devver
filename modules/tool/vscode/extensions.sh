ba227936487702188825d16dcc175382993e4bc9
Natan Sągol <m@merlinnot.com>
2018-02-02 13:15:07 +0000

    Add vscode extensions

--- modules/tool/vscode/extensions.sh
+++ modules/tool/vscode/extensions.sh
@@ -22,20 +22,22 @@ function vsext {
 
 vsext abusaidm html-snippets 0.1.0
 vsext akamud vscode-javascript-snippet-pack 0.1.5
+vsext bierner lit-html 1.2.0
 vsext christian-kohler npm-intellisense 1.3.0
 vsext christian-kohler path-intellisense 1.4.2
 vsext codezombiech gitignore 0.5.0
 vsext cssho vscode-svgviewer 1.4.3
 vsext dbaeumer vscode-eslint 1.4.3
 vsext donjayamanne githistory 0.2.3
-vsext eg2 vscode-npm-script 0.3.3
 vsext eg2 tslint 1.0.24
+vsext eg2 vscode-npm-script 0.3.3
 vsext formulahendry auto-rename-tag 0.0.15
 vsext hoovercj vscode-dimmer 2.1.0
 vsext jakob101 RelativePath 1.0.0
 vsext naumovs color-highlight 2.3.0
 vsext PeterJausovec vscode-docker 0.0.22
 vsext polymer polymer-ide 0.6.0
+vsext redhat vscode-yaml 0.0.8
 vsext waderyan gitblame 2.2.0
 vsext wmaurer change-case 1.0.0
 vsext xabikos JavaScriptSnippets 1.5.0
